8c904af0fc44e3a26b80b129b6a2bbe57bccfdd5
pavana <pavanam665@gmail.com>
2022-11-03 09:27:43 +0530

    First Commit

---
+++ largest.py
@@ -0,0 +1,16 @@
+#find the largest of 2 numbers
+print("enter the first number")
+a=int(input())
+print("enter the second number")
+b=int(input))
+s=a>b
+if(a>b):
+    print("a is greater than b when a is {} and {} is {}".format(a,b))
+if(a,b):
+    print(b is greater than a when a is {} and {} is {".format(a,b))
+
+
+
+
+
+ 
---
+++ triangle.py
@@ -0,0 +1,70 @@
+#area of triangle
+h=int(input("enter the height "))
+print("the height is",h)
+b=int(input("enter the base"))
+print("the base is",b)
+a=1/2*h*b
+print("the area of triangle is",a)
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
